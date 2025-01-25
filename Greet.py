7e17fe3701d85717ef6421b7efd70b1e25eb7e93
mhmukry <mhmukry@gmail.com>
2025-01-25 15:09:17 -0500

    Greet.py commit

---
+++ Greet.py
@@ -0,0 +1,6 @@
+# Class to take user name and greet the user with the good morning message
+class Greet():
+    # Taking input from the user
+    name = input("What is your name? ")
+    # Printing Greeting message for the user
+    print(f'Good Morning {name} ')
---
+++ Greet_version2.py
@@ -0,0 +1,8 @@
+# Class to take user name and greet the user with the good morning message
+class Greet():
+    # Taking input from the user
+    name = input("What is your name? ")
+    # Printing Greeting message for the user
+    for i in range(3):
+        print(f'Good Morning {name} ') 
+    
